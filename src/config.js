d5d08bda2992b9fa2e8b3203ae436cff64cb7a69
Kyle Eriss <keriss@mooveguru.com>
2024-02-16 02:13:02 -0500

    Revising config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://obscure-fortress-21362-cb466f5d50c9.herokuapp.com',
+    API_ENDPOINT: 'https://intense-reef-51693-bf02c1f038f0.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'recipro-client-auth-token',
 };
