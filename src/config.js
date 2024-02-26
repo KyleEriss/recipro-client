aa71ac9c8e2d32d73822ef885274bb1cf97ec8bc
Kyle Eriss <kyle.eriss@gmail.com>
2024-02-26 15:46:17 -0500

    Update config.js API endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://intense-reef-51693-bf02c1f038f0.herokuapp.com',
+    API_ENDPOINT: 'https://mysterious-forest-37033-36099a65b501.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'recipro-client-auth-token',
 };
