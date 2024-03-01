aa00dd7b16f6d70eafab0400466686b70d81bf0a
Kyle Eriss <kyle.eriss@gmail.com>
2024-03-01 17:36:30 -0500

    Modify API_ENDPOINT value

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://mysterious-forest-37033-36099a65b501.herokuapp.com',
+    API_ENDPOINT: 'https://nameless-badlands-13537-59ef1a6f4021.herokuapp.com/',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'recipro-client-auth-token',
 };
