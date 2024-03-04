9343437cd07cdcee59883afc3ce88b7939cb0026
Kyle Eriss <kyle.eriss@gmail.com>
2024-03-04 01:50:57 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://ancient-bastion-18948-1f7fe9b636b4.herokuapp.com',
+    API_ENDPOINT: 'https://api.reciproapp.com',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'recipro-client-auth-token',
 };
