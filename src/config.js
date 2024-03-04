87f2241b1e24b3dbc6368ac23fee93fca5284505
Kyle Eriss <kyle.eriss@gmail.com>
2024-03-03 22:54:35 -0500

    Update API_ENDPOINT value in config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://nameless-badlands-13537-59ef1a6f4021.herokuapp.com',
+    API_ENDPOINT: 'https://ancient-bastion-18948-1f7fe9b636b4.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'recipro-client-auth-token',
 };
