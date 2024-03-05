0971dfd7c267e170e9272792bfa2ed47792895a0
Kyle Eriss <kyle.eriss@gmail.com>
2024-03-04 19:59:17 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://api.reciproapp.com',
+    API_ENDPOINT: 'http://3.16.163.87',
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'recipro-client-auth-token',
 };
